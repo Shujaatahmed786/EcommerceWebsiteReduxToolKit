82c885a441b0badc26dfc0e6ba943e249c8b79df
Shujaat Ahmed <ahmed.shujaat555@gmail.com>
2024-04-22 23:02:53 +0500

    checking routes

--- src/Components/FilteredProduct/FilteredProudct.jsx
+++ src/Components/FilteredProduct/FilteredProudct.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router'
 
 const FilteredProudct = () => {
+  const products = useSelector((state) => state.products.FilteredProudct)
+  console.log(products , 'products')
+  const params = useParams();
+  console.log(params, "params")
   return (
     <div>FilteredProudct</div>
   )
